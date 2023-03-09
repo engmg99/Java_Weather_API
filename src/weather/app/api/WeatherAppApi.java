95951ea3cbffc8459722e4fcce07600232576fed
engmg99 <goyalmunish4306@gmail.com>
2023-03-09 19:25:14 +0530

    java change

---
+++ src/weather/app/api/WeatherAppApi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weather.app.api;
+
+/**
+ *
+ * @author Mishu
+ */
+public class WeatherAppApi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home call = new Home();
+        call.setVisible(true);
+    }
+}
